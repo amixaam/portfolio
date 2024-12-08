d4a84d59b0b03b0a38bbd85c82738625be47a1bb
Roberts Briņķis <60570885+amixaam@users.noreply.github.com>
2024-12-08 13:34:56 +0200

    favicon update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,12 @@
         <title>Roberts Briņķis</title>
         <link rel="icon" href="../favicon.png">
         <meta name="title" content="Roberts Briņķis">
-        <meta name="description" content="My portfolio">
+        <meta name="description" content="Programming has been my favorite activity since I was 10, starting with Scratch and progressing to Python, C#, PHP, JavaScript, and Kotlin. My relentless pursuit of excellence drives my success.">
         <meta name="keywords" content="portfolio">
         <meta name="author" content="Roberts Briņķis">
+
+        <meta name="googlebot" content="all">
+
         @viteReactRefresh 
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         <!-- As you can see, we will use vite with jsx syntax for React-->
